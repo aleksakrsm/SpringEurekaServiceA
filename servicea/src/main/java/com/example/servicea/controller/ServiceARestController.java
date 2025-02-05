2530b089a8f340ba478fb893768ecab8e55f63e8
aleksakrsmanovic <aleksa.krsmanovic@ezdiharfintech.com>
2025-02-05 14:43:59 +0100

    service id enviroment variable

--- servicea/src/main/java/com/example/servicea/controller/ServiceARestController.java
+++ servicea/src/main/java/com/example/servicea/controller/ServiceARestController.java
@@ -1,6 +1,7 @@
 package com.example.servicea.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class ServiceARestController {
 
+    @Value("${service.id}")
+    private String serviceId;
+
     @GetMapping("/helloWorld")
     public String helloWorld() {
         log.info("Hello world from Service A!");
-        return "Hello world from Service A!";
+        return "Hello world from Service A! My name is " + serviceId;
     }
 
 }
